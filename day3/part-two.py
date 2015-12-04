a24e2813647e4518b9f65b9b1459c35568c142db
rmhowe <tiaeph@gmail.com>
2015-12-04 14:21:50 +1100

    Improved day 3 part 2 python solution to be more functional and generic

--- day3/part-two.py
+++ day3/part-two.py
@@ -1,66 +1,67 @@
 #
-# There must be a better way to do this but I'm not sure how right now! At the
-# very least I should be able to split the function up into smaller
-# components more cleanly.
+# Somewhat happier with this solution than my previous one for this part (see
+# git log). Functionality abstracted out into components (functions). Learned
+# about defaultdict which was useful here for storing counts of things in a
+# dictionary. I wish I could use immutable dictionaries to keep track of things
+# so that I could make the code more functional (functions would return a new
+# dictionary rather than mutate an existing one) but this didn't seem easily
+# achievable in python.
+#
+# I also rewrote this to allow for a varying number of deliverers (more than
+# just human and robot Santa) because why not.
 #
 
 import sys
+from collections import defaultdict
 
-def get_presents_delivered(delivery_file):
-    coords = {
-        'human_x': 0,
-        'human_y': 0,
-        'robo_x': 0,
-        'robo_y': 0
-    }
-    human_starting_coords = (coords['human_x'], coords['human_y'])
-    robo_starting_coords = (coords['robo_x'], coords['robo_y'])
+def get_presents_delivered(delivery_file, deliverers):
+    deliverer_coords = defaultdict(int)
+    presents_delivered = defaultdict(int)
 
-    presents_delivered = {}
-    presents_delivered[human_starting_coords] = 1
-    if robo_starting_coords in presents_delivered:
-        presents_delivered[robo_starting_coords] += 1
-    else:
-        presents_delivered[robo_starting_coords] = 1
+    # Add presents delivered for starting coords
+    for deliverer in deliverers:
+        update_presents_delivered(deliverer, deliverer_coords, presents_delivered)
 
-    human_turn = True
+    # Add presents delivered based on file instructions
+    current_deliverer_number = 0
     for line in delivery_file:
         for direction in line:
-            if human_turn:
-                prefix = 'human_'
-            else:
-                prefix = 'robo_'
-
-            update_coords(coords, direction, prefix)
-            update_presents_delivered(presents_delivered, coords, prefix)
-
-            human_turn = not human_turn
+            deliverer = deliverers[current_deliverer_number]
+            update_deliverer_coords(direction, deliverer, deliverer_coords)
+            update_presents_delivered(deliverer, deliverer_coords, presents_delivered)
+            current_deliverer_number = get_new_deliverer_number(len(deliverers), current_deliverer_number)
 
     return presents_delivered
 
-def update_coords(coords, direction, prefix):
+def update_deliverer_coords(direction, deliverer, deliverer_coords):
     if direction == '>':
-        coords[prefix + 'x'] += 1
+        deliverer_coords[(deliverer, 'x')] += 1
     elif direction == '<':
-        coords[prefix + 'x'] -= 1
+        deliverer_coords[(deliverer, 'x')] -= 1
     elif direction == '^':
-        coords[prefix + 'y'] += 1
+        deliverer_coords[(deliverer, 'y')] += 1
     elif direction == 'v':
-        coords[prefix + 'y'] -= 1
+        deliverer_coords[(deliverer, 'y')] -= 1
 
 
-def update_presents_delivered(presents_delivered, coords, prefix):
-    current_coords = (coords[prefix + 'x'], coords[prefix + 'y'])
-    if current_coords in presents_delivered:
-        presents_delivered[current_coords] += 1
+def update_presents_delivered(deliverer, deliverer_coords, presents_delivered):
+    x = deliverer_coords[(deliverer, 'x')]
+    y = deliverer_coords[(deliverer, 'y')]
+    new_coords = (x, y)
+    presents_delivered[new_coords] += 1
+
+def get_new_deliverer_number(num_deliverers, current_deliverer_number):
+    if current_deliverer_number == num_deliverers - 1:
+        return 0
     else:
-        presents_delivered[current_coords] = 1
-
+        return current_deliverer_number + 1
 
 if len(sys.argv) > 1:
     file_name = sys.argv[1]
 else:
     file_name = 'input.txt'
 delivery_file = open(file_name, 'r')
-print len(get_presents_delivered(delivery_file).keys())
+deliverers = ('human', 'robot')
+presents_delivered = get_presents_delivered(delivery_file, deliverers)
+print len(presents_delivered.keys())
 delivery_file.close()
