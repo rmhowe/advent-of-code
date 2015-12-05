0efecbc6acd1e7cc9276542548a54214cf52a400
rmhowe <tiaeph@gmail.com>
2015-12-05 11:12:57 +1100

    Fixes day 4 node solution

--- day4/part-one-two.js
+++ day4/part-one-two.js
@@ -1,5 +1,6 @@
 /*
- * This is currently not working, not sure why...
+ * Forgot to add trim(), very important! Learnt about startsWith() and the
+ * crypto node module.
  */
 
 'use strict';
@@ -8,13 +9,13 @@ const crypto = require('crypto');
 
 let fileName = process.argv[2] || 'input.txt';
 fs.readFile(fileName, (err, data) => {
-  console.log(getKeyPart(String(data)));
+  console.log(getKeyPart(String(data).trim()));
 });
 
 function getKeyPart(secretKey) {
   let keyPart = 0;
   let digest = '';
-  while (!digest.startsWith('00000')) {
+  while (!digest.startsWith('000000')) {
     keyPart++;
     let md5sum = crypto.createHash('md5');
     md5sum.update(secretKey + keyPart);
